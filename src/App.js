3e21170b9885a5f01acefcec677bb6b3f027572a
Neil Chauhan <neilchauhan2@gmail.com>
2020-07-11 14:36:26 +0530

    routing done
    Signed-off-by: Neil Chauhan <neilchauhan2@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import News from "./components/pages/News";
+import Trending from "./components/pages/Trending";
+import Submit from "./components/pages/Submit";
+import Search from "./components/pages/Search";
+import Profile from "./components/pages/Profile";
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <h1>hello World</h1>
-        </div>
+        <Router>
+            <div className="App">
+                {/* Navbar--> */}
+                <Switch>
+                    <Route path="/news" component={News} />
+                    <Route path="/trending" component={Trending} />
+                    <Route path="/submit" component={Submit} />
+                    <Route path="/search" component={Search} />
+                    <Route path="/profile" component={Profile} />
+                    <h1>hello World</h1>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
