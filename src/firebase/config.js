ffe3b961a08379d901401275931ed70d08f2a145
Neil Chauhan <neilchauhan2@gmail.com>
2020-07-11 19:33:50 +0530

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlNyIR-EyPQfzGjWaiIo4uCsmpjOLiSGw",
+    authDomain: "newsapp-114ae.firebaseapp.com",
+    databaseURL: "https://newsapp-114ae.firebaseio.com",
+    projectId: "newsapp-114ae",
+    storageBucket: "newsapp-114ae.appspot.com",
+    messagingSenderId: "810047275369",
+    appId: "1:810047275369:web:7bc50e0c1e936198f47df8"
+};
+
+export default firebaseConfig;
