a3eeb3b15e7b7b2a485573a8c0d8b9bd592defbc
Neil Chauhan <neilchauhan2@gmail.com>
2020-07-14 22:28:46 +0530

    Forgot password functionality done
    Signed-off-by: Neil Chauhan <neilchauhan2@gmail.com>

--- src/components/auth/Forgot.js
+++ src/components/auth/Forgot.js
@@ -1,6 +1,31 @@
 import React from "react";
+import useForm from "../../hooks/useForm";
+import firebase from "../../firebase";
+import alert from "../helpers/alert";
+import validatePasswordReset from "../../validators/validatePasswordReset";
 
 const Forgot = () => {
+    const INITIAL_STATE = {
+        email: ""
+    };
+
+    const handlePasswordReset = async () => {
+        try {
+            const { email } = values;
+            await firebase.resetPassword(email);
+            alert("Check your email for password reset.", "link");
+        } catch (error) {
+            console.error("Password reset error.", error);
+            alert(error.message);
+        }
+    };
+
+    const { handleSubmit, handleChange, values, isSubmitting } = useForm(
+        INITIAL_STATE,
+        validatePasswordReset,
+        handlePasswordReset
+    );
+
     return (
         <div className="container forgot">
             <h1 className="is-size-3 has-text-centered">Forgot Password</h1>
@@ -8,13 +33,18 @@ const Forgot = () => {
                 <label class="label">Email</label>
                 <div class="control">
                     <input
+                        name="email"
+                        onChange={handleChange}
+                        value={values.email}
                         class="input"
                         type="email"
                         placeholder="abc@example.com"
                     />
                 </div>
             </div>
-            <button className="button is-link">Reset Password</button>
+            <button className="button is-link" onClick={handleSubmit}>
+                Reset Password
+            </button>
         </div>
     );
 };
